221a55e5029a408186d7eb3ae1b77c416470dfdd
Yuriy Gomenyuk <yourgo@gmail.com>
2023-04-12 01:22:34 +0300

    cors 2

--- app.js
+++ app.js
@@ -21,7 +21,7 @@ const app = express();
 const formatsLogger = app.get('env') === 'development' ? 'dev' : 'short';
 
 app.use(logger(formatsLogger));
-// app.use(cors());
+app.use(cors());
 app.use(express.json());
 
 app.use('/api/contacts', contactsRouter);
